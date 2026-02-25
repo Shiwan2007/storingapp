f2a888993e6323975327d96a514150245154ff22
Shiwan2007 <D311583@edu.curio.nl>
2026-02-25 09:55:22 +0100

    .

--- app/Http/Controllers/meldingenController.php
+++ app/Http/Controllers/meldingenController.php
@@ -4,16 +4,26 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit'];
 $melder = $_POST['melder'];
-$type = "";
+$type = $_POST['type'];
+$overig = $_POST['overig'];
+$error = array();
+if (isset($_POST['prioriteit'])) 
+{
+    $prioriteit = true;
+}
+else 
+{
+    $prioriteit = false;
+}
 
 echo $attractie . " / " . $capaciteit . " / " . $melder;
-
+if (count(value: $error) == 0) {
 //1. Verbinding
 require_once '../../../config/conn.php';
 
 //2. Query
-$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type)
-VALUES(:attractie, :capaciteit, :melder, :type)";
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type, prioriteit, overig)
+VALUES(:attractie, :capaciteit, :melder, :type, :prioriteit, :overig)";
 
 
 //3. Prepare
@@ -25,7 +35,14 @@ $statement->execute(params: [
  ":capaciteit" => $capaciteit,
  ":melder" => $melder,
  ":type" => $type,
+ ":prioriteit" => $prioriteit,  
+ ":overig" => $overig,
 ]);
 
 header("Location: ../../../resources/views/meldingen/index.php?msg=Melding opgeslagen");
 
+}
+else {
+    var_dump(value: $error);
+    exit();
+    }
--- resources/views/meldingen/create.php
+++ resources/views/meldingen/create.php
@@ -1,15 +1,15 @@
-<?php require_once __DIR__.'/../../../config/config.php'; ?>
+<?php require_once __DIR__ . '/../../../config/config.php'; ?>
 <!doctype html>
 <html lang="nl">
 
 <head>
     <title>StoringApp / Meldingen / Nieuw</title>
-    <?php require_once __DIR__.'/../components/head.php'; ?>
+    <?php require_once __DIR__ . '/../components/head.php'; ?>
 </head>
 
 <body>
 
-    <?php require_once __DIR__.'/../components/header.php'; ?>
+    <?php require_once __DIR__ . '/../components/header.php'; ?>
 
     <div class="container">
         <h1>Nieuwe melding</h1>
@@ -23,14 +23,37 @@
             <div class="form-group">
                 <label for="type">Type</label>
                 <!-- hier komt een dropdown -->
+                 <select name="type" id="type">
+                    <option value=""> - kies je type - </option>
+                    <option value="achtbaan">achtbaan</option>
+                    <option value="draaiend">draaiend</option>
+                    <option value="kinder">kinder</option>
+                    <option value="horeca">horeca</option>
+                    <option value="show">show</option>
+                    <option value="water">water</option>
+                    <option value="overig">overige</option>
+                 </select>
+                 <div class="from-group">
+
+                 </div>
             </div>
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
             </div>
             <div class="form-group">
+                <label for="prioriteit">Prio:</label>
+                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <label for="prioriteit">Melding met prioriteit</label>
+               
+            </div>
+            <div class="form-group">
                 <label for="melder">Naam melder:</label>
                 <input type="text" name="melder" id="melder" class="form-input">
+            </div>
+            <div class="form-group">
+                <label for="overig">Overig:</label>
+                <textarea name="overig" id="overig" class="form-input" rows="4"></textarea>
             </div>
 
             <input type="submit" value="Verstuur melding">
