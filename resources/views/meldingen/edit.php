0f84931ae2b44d873cefe2a95e2addc23f88ce6f
Shiwan2007 <D311583@edu.curio.nl>
2026-03-11 09:12:43 +0100

    Update edit.php

--- resources/views/meldingen/edit.php
+++ resources/views/meldingen/edit.php
@@ -55,26 +55,30 @@
                 <label>Naam attractie:</label>
                 <?php echo $melding['attractie']; ?>
             </div>
+            <div class="form-group">
+                <label>Type attractie:</label>
+                <?php echo $melding['type']; ?>
+            </div>
             <!-- Zorg dat het type wordt getoond, net als de naam hierboven -->
             <div class="form-group">
                 <label for="capaciteit">Capaciteit p/uur:</label>
                 <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input"
-                    value="<?php echo $melding['capaciteit']; ?>">
+                    value="<?php echo $melding['capaciteit'];?>">
             </div>
             <div class="form-group">
                 <label for="prioriteit">Prio:</label>
                 <!-- Let op: de checkbox blijft nu altijd uit, pas dit nog aan -->
-                <input type="checkbox" name="prioriteit" id="prioriteit">
+                <input type="checkbox" name="prioriteit" id="prioriteit" <?php if($melding['prioriteit']) echo 'checked';  ?>>
                 <label for="prioriteit">Melding met prioriteit</label>
             </div>
             <div class="form-group"> 
                 <label for="melder">Naam melder:</label>
                 <!-- Voeg hieronder nog een value-attribuut toe, zoals bij capaciteit -->
-                <input type="text" name="melder" id="melder" class="form-input">
+                <input type="text" name="melder" id="melder" class="form-input" value="<?php echo $melding['melder']; ?>">
             </div>
             <div class="form-group">
                 <label for="overig">Overige info:</label>
-                <textarea name="overig" id="overig" class="form-input" rows="4">.....</textarea>
+                <textarea name="overig" id="overig" class="form-input" rows="4"><?php echo $melding['overig']; ?></textarea>
             </div>
             
             <input type="submit" value="Melding opslaan">
